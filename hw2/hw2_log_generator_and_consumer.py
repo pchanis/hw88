462852a9a7dec9f80e93572893bc9b652f392c78
pchanis <pchanis@vetsfirstchoice.com>
2017-09-17 23:29:14 -0400

    added another test file

---
+++ hw2/hw2_log_generator_and_consumer.py
@@ -0,0 +1,57 @@
+import argparse
+from random import randint
+import Queue
+import threading
+import time
+from multiprocessing import Process, Manager
+from collections import defaultdict
+import datetime
+
+COLUMN_SEPARATOR = " "
+NEW_LINE_CHAR = "\n"
+
+prog = "hw2_4"
+desc = "Generate test log files of the form <timestamp> <userid> <url>"
+
+parser = argparse.ArgumentParser(prog=prog, description=desc)
+parser.add_argument('--logfile','-lf',default='events.txt')
+parser.add_argument('--logfile-count', '-lc', default=4, type=int)
+
+parsed_args = parser.parse_args()
+logfile = parsed_args.logfile
+logfile_count = parsed_args.logfile_count
+
+manager = Manager()
+click_count_by_url_user = manager.defaultdict(dict)
+
+
+def process_log(process_id):
+    logfile_name = process_id + "_" + logfile
+    print "processing log:\t" + logfile_name + NEW_LINE_CHAR
+    with open(logfile_name,"r") as events:
+        for event in events:
+            users = defaultdict(int)
+            values = event.split(" ")
+            if values[1] not in click_count_by_url_user[values[0]]:
+                click_count_by_url_user[values[0]][values[1]] = 1
+            else:
+                click_count_by_url_user[values[0]][values[1]] += 1
+
+jobs=[]
+for process_id in range(logfile_count):
+    p = Process(target=process_log, args=str(process_id))
+    jobs.append(p)
+    p.start()
+
+
+
+for curr_job in jobs:
+    curr_job.join()
+
+print "log processing complete"
+for x in click_count_by_url_user:
+    print (x)
+
+
+
+
