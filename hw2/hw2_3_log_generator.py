81e54c6cde94d6fa24dd2599048d32defc20ef02
pchanis <pchanis@vetsfirstchoice.com>
2017-09-21 12:43:37 -0400

    added another test file

---
+++ hw2/hw2_3_log_generator.py
@@ -0,0 +1,55 @@
+import argparse
+from random import randint
+import Queue
+import threading
+import time
+from multiprocessing import Process
+from collections import defaultdict
+import datetime
+
+COLUMN_SEPARATOR = " "
+NEW_LINE_CHAR = "\n"
+
+prog = "hw2_3_log_generator"
+desc = "Generate test log files of the form <timestamp> <userid> <url>"
+
+parser = argparse.ArgumentParser(prog=prog, description=desc)
+parser.add_argument('--outfile','-f',default='events.txt')
+parser.add_argument('--user-count','-uc',default=10, type=int)
+parser.add_argument('--event-count-user-url','-uuc',default=10, type=int)
+parser.add_argument('--url-file','-uf')
+parser.add_argument('--thread-count', '-tc', default=4, type=int)
+
+parsed_args = parser.parse_args()
+outfile = parsed_args.outfile
+user_count = parsed_args.user_count
+events_per_user_url = parsed_args.event_count_user_url
+url_list_file = parsed_args.url_file
+thread_count = parsed_args.thread_count
+
+
+def create_event_log_file(log_file):
+    event_log_file = open(log_file,"w")
+    with open(url_list_file,"r") as urls:
+        for url in urls:
+            url = url.strip()
+            for user_number in range(user_count):
+                user_name = "usr" + str(user_number)
+                for event_num in range(events_per_user_url):
+                    currentTime = str(datetime.datetime.now())
+                    event_log_file.write(currentTime + COLUMN_SEPARATOR + url + COLUMN_SEPARATOR + user_name +NEW_LINE_CHAR)
+    event_log_file.close()
+
+
+jobs=[]
+for process_id in range(thread_count):
+    logfilename = str(process_id) + "_" + outfile
+    p = Process(target=create_event_log_file, args=(logfilename,))
+    jobs.append(p)
+    p.start()
+
+for curr_job in jobs:
+    curr_job.join()
+
+print "process complete"
+
---
+++ hw2/hw2_4_og_processor_multithreaded_list.py
@@ -0,0 +1,112 @@
+import argparse
+from random import randint
+import Queue
+import threading
+import time
+from multiprocessing import Process
+import threading
+from collections import defaultdict, Counter
+import datetime
+
+COLUMN_SEPARATOR = " "
+NEW_LINE_CHAR = "\n"
+TAB_CHAR = "\t"
+
+prog = "hw2_4_log_processor"
+desc = "Calculate click counts metrics from raw input log files"
+
+parser = argparse.ArgumentParser(prog=prog, description=desc)
+parser.add_argument('--logfile','-lf',default='events.txt')
+parser.add_argument('--logfile-count', '-lc', default=4, type=int)
+
+parsed_args = parser.parse_args()
+logfile = parsed_args.logfile
+logfile_count = parsed_args.logfile_count
+
+list_dicts = []
+
+#process the raw input files
+def process_log(process_id, process_dict):
+    logfile_name = process_id + "_" + logfile
+    with open(logfile_name,"r") as events:
+        for event in events:
+            event = event.strip(NEW_LINE_CHAR)
+            values = event.split(" ")
+            url = values[2]
+            userid = values[3]
+            if userid not in process_dict[url]:
+                process_dict[url][userid] = 1
+            else:
+                process_dict[url][userid] += 1
+
+#Calculate for query 1
+def count_unique_urls():
+    urls = []
+    for d in list_dicts:
+        urls.extend(list(d))
+    unique_urls = set(urls)
+    return len(unique_urls)
+
+#Calcualte for query 2
+def count_unique_visitors_by_url():
+    users_by_url = defaultdict(list)
+    for d in list_dicts:
+        for k in d.keys():
+            for u in d[k]:
+                users_by_url[k].append(u)
+    for k in users_by_url.keys():
+        users_by_url[k] = set(users_by_url[k])
+        print k + TAB_CHAR + str(len(users_by_url[k]))
+
+#Calculate query 3
+def count_unique_userid_clicks_by_url():
+    count_unique_clicks_by_user_by_url = defaultdict(dict)
+    for d in list_dicts:
+        for k in d.keys():
+            for u,v in d[k].items():
+                if u not in count_unique_clicks_by_user_by_url[k]:
+                    count_unique_clicks_by_user_by_url[k][u] = v
+                else:
+                    count_unique_clicks_by_user_by_url[k][u] += v
+    for k in count_unique_clicks_by_user_by_url.keys():
+         print k + ":"
+         for u,v in count_unique_clicks_by_user_by_url[k].items():
+             print u + TAB_CHAR + str(v)
+
+
+def query_results():
+    #Query 1
+    print "Query 1: unique URLs:\t" + str(count_unique_urls())
+
+    #Query 2
+    print "Query 2: unique visitors by URL:"
+    count_unique_visitors_by_url()
+
+    #Query 3
+    print "Query 3: unique user click counts by URL:"
+    count_unique_userid_clicks_by_url()
+
+
+#set up the separate threads
+jobs=[]
+for process_id in range(logfile_count):
+    list_dicts.append(defaultdict(dict)) #add a dictionary for each thread
+    #call function to process one log and pass in dictionary
+    t = threading.Thread(target=process_log, args=(str(process_id), list_dicts[process_id]))
+    jobs.append(t)
+
+for job in jobs:
+    job.start()
+
+for curr_job in jobs:
+    curr_job.join()
+
+print "log processing complete"
+
+#calculate and print the query results
+query_results()
+
+
+
+
+
