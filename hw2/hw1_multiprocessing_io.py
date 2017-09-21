b9a55ee0e6b82d2fb592d886c75874205ec85aa5
pchanis <pchanis@vetsfirstchoice.com>
2017-09-20 23:10:28 -0400

    added another test file

--- hw2/hw1_multiprocessing_io.py
+++ hw2/hw1_multiprocessing_io.py
@@ -19,26 +19,25 @@ parser = argparse.ArgumentParser(prog=prog, description=desc)
 # parser.add_argument('--line-count','-l',default=10, type=int)
 # parser.add_argument('--column-count','-c',default=3, type=int)
 # parser.add_argument('--file-count','-fc',default=5, type=int)
-parser.add_argument('--thread-count','-tc',default=5, type=int)
+parser.add_argument('--process-count','-pc',default=5, type=int)
 parser.add_argument('--read_file','-rf', default="file.txt")
 
 parsed_args = parser.parse_args()
-thread_count = parsed_args.thread_count
+process_count = parsed_args.process_count
 read_file = parsed_args.read_file
 
 
 def io_intensive(proc_id):
     while 1:
         with open(read_file, "r") as in_file, open(read_file + "copy" + str(proc_id),"w") as write_file:
-            time.sleep(0.001)
             for line in in_file:
+                time.sleep(0.001)
                 write_file.write(line)
 
 
-
 processes=[]
-for thread_count_id in range(thread_count):
-    p = Process(target=io_intensive, args=(thread_count,))
+for process_count_id in range(process_count):
+    p = Process(target=io_intensive, args=(process_count_id,))
     p.start()
     processes.append(p)
 
