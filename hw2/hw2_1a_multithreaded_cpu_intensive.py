50d659e2f5aba08ff5bb8cc427947b44813237e6
pchanis <pchanis@vetsfirstchoice.com>
2017-09-21 22:39:17 -0400

    added another test file

---
+++ hw2/hw2_1a_multithreaded_cpu_intensive.py
@@ -0,0 +1,47 @@
+import argparse
+from random import randint
+import Queue
+import threading
+import time
+from multiprocessing import Process, Pool
+import re
+import hashlib
+
+COLUMN_SEPARATOR = " "
+NEW_LINE_CHAR = "\n"
+
+prog = "hw2_1a_multiprocessing_cpu_intensive"
+desc = "Generate multiprocess cpu intensive load by calculating md5 on a file"
+
+parser = argparse.ArgumentParser(prog=prog, description=desc)
+parser.add_argument('--thread-count','-tc',default=5, type=int)
+parser.add_argument('--sleep-time','-st', default=0.015, type=float)
+
+parsed_args = parser.parse_args()
+thread_count = parsed_args.thread_count
+sleep_time = parsed_args.sleep_time
+
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+
+
+def cpu_intensive():
+    while 1:
+        time.sleep(sleep_time)
+        md5("cpu_file.txt")
+
+
+processes=[]
+for thread_count_id in range(thread_count):
+    p = threading.Thread(target=cpu_intensive)
+    p.start()
+    processes.append(p)
+
+for curr_proc in processes:
+    curr_proc.join()
+print "process complete"
+
